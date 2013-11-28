cbf8fa2cb61a2ab701f63ae09819e92201273174
sorafeng <sorafeng@gmail.com>
2013-11-28 17:27:02 -0500

    Update the SQL Script to verify the RecipeState is active

--- RecipeBankWebServices/src/com/rb/webServices/Recipe.java
+++ RecipeBankWebServices/src/com/rb/webServices/Recipe.java
@@ -22,7 +22,7 @@ public class Recipe {
 
 	//Create by Dongchao Feng
 	public String getAllRecipes() {
-		String sql = "select * from recipebank.recipe";
+		String sql = "select * from recipebank.recipe where RecipeState = 0";
 		try {
 			conn = ConnectDB.getConnection();
 			st = conn.prepareStatement(sql);
@@ -40,7 +40,7 @@ public class Recipe {
 	//Create by Dongchao Feng
 	public String getRecipeByAccount(int accountId) {
 		String recipeString = "";
-		String sql = "select RecipeId,RecipeTitle,Description,rate,RecipeState,a.AccountId,NickName from recipebank.recipe r join recipebank.account a on r.AccountID=a.AccountId where a.AccountId="
+		String sql = "select RecipeId,RecipeTitle,Description,rate,RecipeState,a.AccountId,NickName from recipebank.recipe r join recipebank.account a on r.AccountID=a.AccountId where r.RecipeState = 0 and a.AccountId="
 				+ accountId + " order by RecipeId desc";
 		try {
 			conn = ConnectDB.getConnection();
@@ -61,7 +61,7 @@ public class Recipe {
 	public String getRecipeByAccountWithAmount(int accountId, int amount) {
 		String recipeString = "";
 		String sql = "select RecipeId,RecipeTitle,Description,rate,RecipeState,a.AccountId,NickName "
-				+ "from recipebank.recipe r join recipebank.account a on r.AccountID=a.AccountId where a.AccountId="
+				+ "from recipebank.recipe r join recipebank.account a on r.AccountID=a.AccountId where r.RecipeState = 0 and a.AccountId="
 				+ accountId + " order by RecipeId desc limit " + amount;
 		try {
 			conn = ConnectDB.getConnection();
@@ -100,7 +100,7 @@ public class Recipe {
 	//Create by Dongchao Feng
 	public String getRecipeDetails(int recipeId) {
 
-		String sql = "select * from recipebank.recipestep  where RecipeId="
+		String sql = "select * from recipebank.recipestep s inner join recipebank.recipe r on s.RecipeId = r.RecipeId where r.RecipeState=0 and s.RecipeId="
 				+ recipeId;
 		try {
 			conn = ConnectDB.getConnection();
@@ -117,7 +117,7 @@ public class Recipe {
 	
 	//Create by Dongchao Feng
 	public String getRecipeFromFavouriteList(int recipeId){
-		String sql = "select * from recipebank.recipe where RecipeId ="
+		String sql = "select * from recipebank.recipe where RecipeState=0 and RecipeId ="
 				+ recipeId;
 		try {
 			conn = ConnectDB.getConnection();
@@ -135,7 +135,7 @@ public class Recipe {
 
 	//Create by Dongchao Feng
 	public String searchRecipeById(int recipeId) {
-		String sql = "select * from recipe inner join recipecategory on recipe.RecipeId = recipecategory.RecipeId inner join category on recipecategory.CategoryId = category.CategoryId where recipe.RecipeId="
+		String sql = "select * from recipe inner join recipecategory on recipe.RecipeId = recipecategory.RecipeId inner join category on recipecategory.CategoryId = category.CategoryId where recipe.RecipeState=0 and recipe.RecipeId="
 				+ recipeId;
 		try {
 			conn = ConnectDB.getConnection();
@@ -153,7 +153,7 @@ public class Recipe {
 
 	//Create by Dongchao Feng
 	public String searchRecipeByCategory(int categoryId) {
-		String sql = "select * from recipe inner join recipecategory on recipe.RecipeId = recipecategory.RecipeId inner join category on recipecategory.CategoryId = category.CategoryId where category.CategoryId="
+		String sql = "select * from recipe inner join recipecategory on recipe.RecipeId = recipecategory.RecipeId inner join category on recipecategory.CategoryId = category.CategoryId where recipe.RecipeState=0 and category.CategoryId="
 				+ categoryId;
 		try {
 			conn = ConnectDB.getConnection();
@@ -170,7 +170,7 @@ public class Recipe {
 
 	//Create by Dongchao Feng
 	public String searchRecipeByRate(int rate) {
-		String sql = "select * from recipe inner join recipecategory on recipe.RecipeId = recipecategory.RecipeId inner join category on recipecategory.CategoryId = category.CategoryId where recipe.Rate="
+		String sql = "select * from recipe inner join recipecategory on recipe.RecipeId = recipecategory.RecipeId inner join category on recipecategory.CategoryId = category.CategoryId where  recipe.RecipeState=0 and recipe.Rate="
 				+ rate;
 		try {
 			conn = ConnectDB.getConnection();
@@ -204,7 +204,7 @@ public class Recipe {
 
 	//Create by Dongchao Feng
 	public String searchRecipeByKeyWord(String keyword) {
-		String sql = "select * from recipe inner join recipecategory on recipe.RecipeId = recipecategory.RecipeId inner join category on recipecategory.CategoryId = category.CategoryId where recipe.RecipeTitle LIKE '%"
+		String sql = "select * from recipe inner join recipecategory on recipe.RecipeId = recipecategory.RecipeId inner join category on recipecategory.CategoryId = category.CategoryId where  recipe.RecipeState=0 and recipe.RecipeTitle LIKE '%"
 				+ keyword
 				+ "%'or recipe.Description LIKE '%"
 				+ keyword
@@ -227,7 +227,7 @@ public class Recipe {
 	//Create by Dongchao Feng
 	public String getRecipeIngredient(int recipeId)
 	{
-		String sql = "select recipe.RecipeId,recipe.RecipeTitle,a.IngredientId,a.ingredientMeasure,a.IngredientQuanlity,b.IngredientName from recipebank.recipe inner join recipebank.recipeingredientlist a on recipe.RecipeId = a.RecipeId inner join recipebank.ingredient b on a.ingredientId = b.ingredientId where recipe.RecipeId="
+		String sql = "select recipe.RecipeId,recipe.RecipeTitle,a.IngredientId,a.ingredientMeasure,a.IngredientQuanlity,b.IngredientName from recipebank.recipe inner join recipebank.recipeingredientlist a on recipe.RecipeId = a.RecipeId inner join recipebank.ingredient b on a.ingredientId = b.ingredientId where  recipe.RecipeState=0 and recipe.RecipeId="
 				+ recipeId;
 		try {
 			conn = ConnectDB.getConnection();
@@ -307,7 +307,7 @@ public class Recipe {
 		try {
 			conn = ConnectDB.getConnection();
 			PreparedStatement st = null;
-			String sql = "update recipebank.recipe set Rate = ? where RecipeId=?";
+			String sql = "update recipebank.recipe set Rate = ? where RecipeState=0 and  RecipeId=?";
 			st = conn.prepareStatement(sql);
 			st.setInt(1, rate);
 			st.setInt(2, recipeId);
